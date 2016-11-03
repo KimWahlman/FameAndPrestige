9b66be57fb8b0b7210b7c91e8fbed5b647e81a34
kimwahlman <kim.wahlman@gmail.com>
2016-11-03 15:17:59 +0100

    Iann wanted to wreck the code.

--- Assets/Source/Deck.cs
+++ Assets/Source/Deck.cs
@@ -5,15 +5,40 @@ public class Deck : MonoBehaviour {
 
     public GameObject prefab;
     public bool mouseOver = false;
-    
+    public ArrayList deck = new ArrayList();
+    public int currentNumOfCardsInDeck;
+    public int[] theDeck = new int[50];
+    public string[] msg = new string[3]; // example ["DRAW", "1", "3"] // Action, somthing, amount of cards to draw
     /*
      * TODO: Make the drawn card random
      * Hold an array that keep track of how many 
      * of a specific card is still in the deck.
-     */
+     
+    
+    void HandleMessage()
+    {
+        switch (msg[0])
+        {
+            case "DRAW":
+                DrawCard()
+                break;
+            default:
+                break;
+        }
+    }
 
+    */
     // Use this for initialization
-    void Start () { }
+    void Start ()
+    {
+        currentNumOfCardsInDeck = 50;
+        for (int i = 0; i < currentNumOfCardsInDeck; i++)
+        {
+            int temp = Random.Range(1, 3);
+            deck.Add(temp);
+            //theDeck
+        }
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -24,12 +49,18 @@ public class Deck : MonoBehaviour {
 
     void CreateNewCard()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && deck.Count > 0)
         {
-            int randomNum = Random.Range(1, 3); // returns a random number between 1 and 2.
+            Debug.Log("1. deck size before: " + deck.Count + " deck size after: " + deck.Count);
             Instantiate(prefab, new Vector3(0.0f, -5.0f, 0.0f), Quaternion.identity);
-            prefab.GetComponent<SpriteRenderer>().sprite = Resources.Load("Images/Card" + randomNum, typeof(Sprite)) as Sprite;
-        }        
+            Debug.Log("2. deck size before: " + deck.Count + " deck size after: " + deck.Count);
+            prefab.GetComponent<SpriteRenderer>().sprite = Resources.Load("Images/Card" + deck[deck.Count - 1], typeof(Sprite)) as Sprite;
+            Debug.Log("3. deck size before: " + deck.Count + " deck size after: " + deck.Count);
+            deck.RemoveAt(deck.Count - 1);
+            Debug.Log("4. deck size before: " + deck.Count + " deck size after: " + deck.Count);
+        }
+        else if (deck.Count == 0)
+            Debug.Log("We are out of cards!");
     }
 
     void OnMouseOver() { if (!mouseOver) mouseOver = true; }
