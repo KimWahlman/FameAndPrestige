a0270e97c6729b6737a9412eef71ad19ec61dd19
Abelarm <luigi3000@gmail.com>
2016-11-11 21:16:55 +0100

    Make it modular ;)

--- TestServer/app.js
+++ TestServer/app.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var counter = require('/pointCounter.js')
 
 var clients = {};
 var cards = {};
