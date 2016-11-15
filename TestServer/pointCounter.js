49620a4adbaa3b2b0bd8fa58c56bb66dfc00eb15
Abelarm <luigi3000@gmail.com>
2016-11-15 12:02:40 +0100

    Moved the build foloders and files into the buil directory so can be ignored

--- TestServer/pointCounter.js
+++ TestServer/pointCounter.js
@@ -1,55 +1,3 @@
-var cards = {};
-
-cards["0"]= {
-    name: "0",
-    description: "1",
-    theme : ["horror", "history"],
-    point: "1",
-    type: "mixed"
-}
-
-cards["1"]= {
-	name: "1",
-	description: "1",
-    theme : ["horror", "nature"],
-    point: "1",
-	type: "mixed"
-}
-cards["2"]= {
-	name: "2",
-	description: "2",
-    theme : ["horror"],
-    point: "1",
-	type: "mixed"
-}
-cards["3"]= {
-	name: "3",
-	description: "3",
-    theme : ["nature", "history"],
-    point: "1",
-	type: "mixed"
-}
-cards["4"]= {
-	name: "4",
-	description: "4",
-    theme : ["horror"],
-    point: "2",
-	type: "pure"
-}
-cards["5"]= {
-	name: "5",
-	description: "5",    
-    theme : ["nature"],
-    point: "2",
-	type: "pure"
-}
-cards["6"]= {
-    name: "5",
-    description: "5",    
-    theme : ["folklore"],
-    point: "2",
-    type: "pure"
-}
 
 module.exports = function checkLinks(cards){
     if(cards.length <= 2){
@@ -167,8 +115,3 @@ module.exports = function countPoints(cards, theme){
 
     	return {normalPoint: normalPoint, comboPoint: comboPoint}
 }
-
-var c = [cards["3"],cards["1"],cards["3"],cards["1"]];
-
-if(checkLinks(c))
-    console.log(countPoints(c, "horror"))
