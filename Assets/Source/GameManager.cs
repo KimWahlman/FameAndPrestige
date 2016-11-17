cff3eab201e1fee4f3278d2467127171a9a6c13d
Abelarm <luigi3000@gmail.com>
2016-11-17 19:27:37 +0100

    Reverting to state of the project at 8ac8229

--- Assets/Source/GameManager.cs
+++ Assets/Source/GameManager.cs
@@ -113,7 +113,7 @@ public class GameManager : MonoBehaviour {
     {
         if(playerID == myPlayer.idPlayer)
         {
-            print("player iD : " + playerID + " card id :  " + cardID);
+            print("DRAW CARD : player iD : " + playerID + " card id :  " + cardID);
             //add the card in the player hand
             myPlayer.cardsHeld.Add(cardID, Deck[cardID]);
 
@@ -122,7 +122,7 @@ public class GameManager : MonoBehaviour {
         }
         else
         {
-            print("player iD : " + playerID + " card id :  " + cardID);
+            print("DRAW CARD : player iD : " + playerID + " card id :  " + cardID);
             myPlayer.opponents[playerID].cardsHeld.Add(cardID, Deck[cardID]);
 
             Deck[cardID].popCard(false, playerHands[playerID].newCard(), playerID);
@@ -132,13 +132,14 @@ public class GameManager : MonoBehaviour {
 
 
     //should be called when a card is played/discarded
-    public void ReOrderPlayerHand(int playerID, int cardID)
+    public void ReOrderPlayerHand(int playerID, List<int> cardIDs)
     {
         print(" in reorderhand ");
 
         if (playerID == myPlayer.idPlayer)
         {
-            myPlayer.cardsHeld.Remove(cardID);
+            foreach (int cardID in cardIDs)
+                myPlayer.cardsHeld.Remove(cardID);
 
             int id = 0;
             foreach (KeyValuePair<int, Card> card in myPlayer.cardsHeld)
@@ -148,7 +149,8 @@ public class GameManager : MonoBehaviour {
         }
         else
         {
-            myPlayer.opponents[playerID].cardsHeld.Remove(cardID);
+            foreach (int cardID in cardIDs)
+                myPlayer.opponents[playerID].cardsHeld.Remove(cardID);
 
             int id = 0;
             foreach (KeyValuePair<int, Card> card in myPlayer.opponents[playerID].cardsHeld)
@@ -156,13 +158,23 @@ public class GameManager : MonoBehaviour {
                 card.Value.PositionOnTheHand(playerHands[playerID].posList[id++]);
             }
         }
-        playerHands[playerID].deadCard();
+
+        foreach(var c in cardIDs)
+            playerHands[playerID].deadCard();
     }
 
     public void storeCard(int cardID)
     {
         toPlay.Add(cardID);
 		playableZone.addCard ();
+    }
+
+    public void removeStoredCard(int cardID)
+    {
+        if(toPlay.Contains(cardID))
+            toPlay.Remove(cardID);
+
+        playableZone.removeCard();
     }
 
     public void sendStoredCards()
@@ -175,9 +187,10 @@ public class GameManager : MonoBehaviour {
         Deck[cardID].playCard();
         
         if(!Deck[cardID].isMine)
+        {
             Deck[cardID].PositionOnTheHand(playableZone.getSlot());
-
-        //playableZone.addCard();
+            playableZone.addCard();
+        }
     }
     
     public void InvalidCardPlayed(int cardID)
