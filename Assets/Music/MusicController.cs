683399b8ddf54764def5d045b8a3ad100eddca1f
kimwahlman <kim.wahlman@gmail.com>
2016-11-29 14:57:04 +0100

    Scene with music.

--- Assets/Music/MusicController.cs
+++ Assets/Music/MusicController.cs
@@ -2,49 +2,54 @@
 using System.Collections;
 using UnityEngine.Audio;
 
+
+/*
+ * TODO (KIM): Fix so the audio isn't playing all the time? 
+ */
+
 public class MusicController : MonoBehaviour {
 
-    public AudioMixerSnapshot normalGameplay, duelGameplay, shameGameplay;
-    public AudioClip[] stings;
-    public AudioSource stingSource;
+    public AudioMixerSnapshot normalGameplay, duelGameplay, shameGameplay, silence;
+    //public AudioClip[] stings;
+    public AudioSource[] mSource;
     public float bpm = 128;
-
     private float mTransitionIn, mTransitionOut, mQuarterNote;
-	public int mCurrentGameMode = 0;
+    public string mCurrentGameMode;
 	// Use this for initialization
 	void Start ()
     {
         mQuarterNote = 60 / bpm;
         mTransitionIn = mQuarterNote;
         mTransitionOut = mQuarterNote * 32;
-        mCurrentGameMode = 0;
+        mCurrentGameMode = "normal";
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
-	}
+
+    }
 
     void FixedUpdate()
     {
-        if(mCurrentGameMode == 0)
+        if (mCurrentGameMode == "normal") // 0
         {
+            
             normalGameplay.TransitionTo(mTransitionIn);
         }
-        else if(mCurrentGameMode == 1)
+        else if (mCurrentGameMode == "duel") // 1
         {
+
             duelGameplay.TransitionTo(mTransitionIn);
         }
-        else if(mCurrentGameMode == 2)
+        else if (mCurrentGameMode == "shame") // 2
         {
             shameGameplay.TransitionTo(mTransitionIn);
         }
         else
         {
-            normalGameplay.TransitionTo(mTransitionOut);
-            duelGameplay.TransitionTo(mTransitionOut);
-            shameGameplay.TransitionTo(mTransitionOut);
+            Debug.Log("Hmpf");
+            silence.TransitionTo(0.25f);
         }
     }
 
