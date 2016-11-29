9a862977d91dd65b59a7bab2d140c783847edbd9
kimwahlman <kim.wahlman@gmail.com>
2016-11-29 14:11:22 +0100

    Added fugly code to play music depending on theme.

---
+++ Assets/Music/MusicController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class MusicController : MonoBehaviour {
+
+    public AudioMixerSnapshot normalGameplay, duelGameplay, shameGameplay;
+    public AudioClip[] stings;
+    public AudioSource stingSource;
+    public float bpm = 128;
+
+    private float mTransitionIn, mTransitionOut, mQuarterNote;
+	public int mCurrentGameMode = 0;
+	// Use this for initialization
+	void Start ()
+    {
+        mQuarterNote = 60 / bpm;
+        mTransitionIn = mQuarterNote;
+        mTransitionOut = mQuarterNote * 32;
+        mCurrentGameMode = 0;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+        if(mCurrentGameMode == 0)
+        {
+            normalGameplay.TransitionTo(mTransitionIn);
+        }
+        else if(mCurrentGameMode == 1)
+        {
+            duelGameplay.TransitionTo(mTransitionIn);
+        }
+        else if(mCurrentGameMode == 2)
+        {
+            shameGameplay.TransitionTo(mTransitionIn);
+        }
+        else
+        {
+            normalGameplay.TransitionTo(mTransitionOut);
+            duelGameplay.TransitionTo(mTransitionOut);
+            shameGameplay.TransitionTo(mTransitionOut);
+        }
+    }
+
+}
+
