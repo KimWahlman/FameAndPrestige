d7bcb8c1e44f25428005bfe7101aba5a1080d1bb
r.diflorio <r.diflorio93@gmail.com>
2016-12-08 18:44:44 +0100

    New Scene working of the book with the wikipedia API

--- Assets/Source/Menu/ShowInformation.cs
+++ Assets/Source/Menu/ShowInformation.cs
@@ -17,80 +17,124 @@ public class ShowInformation : MonoBehaviour {
 	public GameObject parpreb;
 	public GameObject content;
 
+
 	private string url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=";
 	private string infPaint = "";
 	private string infAuth = "";
+	private GameObject desPainting;
+	private GameObject element;
 
 
 
 	// Use this for initialization
-	void Start () {
+	IEnumerator Start () {
 
 		cardButton.onClick.AddListener (testOnClick);
-		LoadInformation ();
-	
+		StartCoroutine(LoadInformation ());
+		yield return 0;
+
 	
 	}
 
-	void LoadInformation(){
 
 
-
-
-
+	IEnumerator LoadInformation(){
+		
 		GameObject imgHeading =  (GameObject)Instantiate (h2preb,new Vector3(0,0,0), Quaternion.identity);  
-		GameObject element = (GameObject)Instantiate (content,informationList.transform.position, informationList.transform.rotation);
+		element = (GameObject)Instantiate (content,new Vector3(0,0,0), Quaternion.identity);
 		GameObject title = (GameObject)Instantiate (h1preb, new Vector3(0,0,0), Quaternion.identity);
 		GameObject  headingPaint =(GameObject)Instantiate (h3preb,new Vector3(0,0,0), Quaternion.identity);
-		GameObject desPainting = (GameObject)Instantiate (parpreb,new Vector3(0,0,0), Quaternion.identity);
-		StartCoroutine (requestInformation (namePainting[0], infPaint));
+		desPainting = (GameObject)Instantiate (parpreb,new Vector3(0,0,0), Quaternion.identity);
+
+		string str = namePainting [0];
+		str = str.Replace (" ","_");
+		url = url + str;
+		//Debug.Log (url);
+		WWW www = new WWW(url);
+		yield return www;
+
+		JSONObject jso = new JSONObject (www.text);
+	
+
+		if (jso ["query"] ["pages"].HasField ("-1")) {
+			//Debug.Log ("missing");		
+
+
+
+		}
+		else {
+
+			infPaint = jso["query"]["pages"][0]["extract"].ToString();
+
+		}
+		//Debug.Log (infPaint.ToString());
+
 
 
 		element.name = name;
 
 		title.GetComponent<Text> ().text = titleCard;
 		imgHeading.GetComponent<Text>().text = "Image information";
-		desPainting.GetComponent<Text> ().text = infPaint;
-		//headingPaint.GetComponent<Text> ().text = namePainting [0];
+		desPainting.GetComponent<Text> ().text = infPaint.ToString();
+		headingPaint.GetComponent<Text> ().text = namePainting [0];
+
+		//Debug.Log (desPainting.GetComponent<Text> ().text);
 
 		title.transform.SetParent (element.transform,false);
 		imgHeading.transform.SetParent (element.transform, false);
 		headingPaint.transform.SetParent (element.transform, false);
-			
+		desPainting.transform.SetParent(element.transform, false);
 
-
-
-
-
-		
 	
 		element.transform.SetParent (informationList.GetComponentInChildren<ScrollRect> ().content, false);
-
-
 		element.SetActive (false);
 
 	}
 
 	void testOnClick(){
 		Debug.Log ("clicked");
+		ScrollRect tmp = informationList.GetComponentInChildren<ScrollRect> ();
+
+		for( int i = 0; i <tmp.content.transform.childCount; ++i )
+		{
+			tmp.content.transform.GetChild(i).gameObject.SetActive(false);
+		}
+		/*
+		for (int i = 0; i < element.transform.childCount; ++i) {
+			element.transform.GetChild (i).gameObject.SetActive (true);
+		}*/
+		element.SetActive (true);
+
+
+
 	}
 
-
-
-	IEnumerator requestInformation(string str, string infPaint) {
+	/*
+	IEnumerator requestInformation(string str, GameObject ob) {
 		str = str.Replace (" ","_");
 		url = url + str;
-		Debug.Log (url);
+		//Debug.Log (url);
 		WWW www = new WWW(url);
-
+		yield return www;
 		//Debug.Log (www.text);
+
 		JSONObject jso = new JSONObject (www.text);
 		//JSONObject id =  jso["query"]["pages"][0];
-		infPaint = jso["query"]["pages"][0]["extract"].ToString();
-		yield return www;
-		//Debug.Log (jso ["query"] ["pages"] [id] ["extract"].ToString());
+		Debug.Log (jso);
+		Debug.Log (jso ["query"] ["pages"]);
+
+		if (jso ["query"] ["pages"].HasField ("-1")) {
+			//Debug.Log ("missing");		
+		
+		}
+		else {
+			//Debug.Log (jso);
+			ob.GetComponent<Text>().text = jso["query"]["pages"][0]["extract"].ToString();
+			//return infPaint;
+			//Debug.Log (infPaint);
+		}
 
 	}
-	
+	*/
 
 }
