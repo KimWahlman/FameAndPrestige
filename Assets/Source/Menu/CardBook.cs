9916ffc0f80d995d0d0e9d79d3e246908a9f0153
r.diflorio <r.diflorio93@gmail.com>
2016-12-08 15:54:51 +0100

    cardbook scene, still not working

---
+++ Assets/Source/Menu/CardBook.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class CardBook : MonoBehaviour {
+	public GameObject deckList;
+	public GameObject informationList;
+	public GameObject menu;
+	public GameObject book;
+	public GameObject  cardElement;
+	public GameObject h1Preb;
+	public GameObject h2Preb;
+	public GameObject h3Preb;
+	public GameObject parPreb;
+	public Button goback;
+	public Button showCitation;
+	public Button showPainting;
+	private string url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=";
+	private string[] titles;
+	private string[] author;
+
+
+	// Use this for initialization
+	void Start () {
+		/*
+		goback.onClick.AddListener (testOnClick);
+		showCitation.onClick.AddListener (testOnClick);
+		showPainting.onClick.AddListener (testOnClick);
+		*/
+		loadCardList ();
+		//StartCoroutine( requestInformation ("Romanticism"));
+	}
+
+	IEnumerator requestInformation(string name) {
+		url = url + name;
+		//Debug.Log (url);
+		WWW www = new WWW(url);
+		yield return www;
+		//Debug.Log (www.text);
+		JSONObject jso = new JSONObject (www.text);
+		//JSONObject id =  jso["query"]["pages"][0];
+		Debug.Log ( jso["query"]["pages"][0]["extract"]);
+		//Debug.Log (jso ["query"] ["pages"] [id] ["extract"].ToString());
+
+	}
+
+	void testOnClick(){
+		Debug.Log ("clicked");
+	}
+
+	void showInformation(string name){
+		informationList.SetActive (true);
+	}
+
+	void goBack(){
+		//resetCanvas ();
+		book.SetActive (false);
+		menu.SetActive(true);
+	}
+	void resetCanvas(){
+	}
+
+	public void loadCardList(){
+		
+
+		string json = Resources.LoadAll("card_text")[0].ToString();
+		JSONObject cards = new JSONObject(json);
+
+		foreach(JSONObject card_json in cards.list){
+
+			GameObject card = (GameObject)Instantiate (cardElement,new Vector3(0,0,0), Quaternion.identity);
+
+
+			string title = card_json ["name"].ToString().Trim(new Char[] { ' ', '"' });
+			string description = card_json ["description"].ToString().Trim(new Char[] { ' ', '"', '\n' });
+			string path = card_json ["img"].ToString().Trim(new Char[] { ' ', '"' });
+			string point = card_json ["point"].ToString().Trim(new Char[] { ' ', '"' });
+			string type = card_json["type"].ToString().Trim(new Char[] { ' ', '"' });
+			string tmp;
+			Debug.Log (card_json);
+			if (type == "pure") {
+			
+				titles = new string[1];
+				author = new string[1];
+				tmp = card_json ["title"].ToString ().Trim (new Char[] { ' ', '"' });
+				if(tmp == "")
+					tmp = "Unknow";
+				titles [0] = tmp;
+
+				tmp = card_json ["author"].ToString ().Trim (new Char[] { ' ', '"' });
+				if(tmp == "")
+					tmp = "Unknow";
+				author [0] = tmp;
+			
+			} else {
+				
+				titles = new string[2];
+				author = new string[2];
+				tmp =card_json ["title"][0].ToString ().Trim (new Char[] { ' ', '"' });
+				if (tmp == "")
+					tmp = "Unknow";
+				titles [0] = tmp;
+					
+				tmp =card_json ["title"][1].ToString ().Trim (new Char[] { ' ', '"' });
+				if (tmp == "")
+					tmp = "Unknow";
+				titles [1] = tmp;
+
+
+				tmp = card_json ["author"][0].ToString ().Trim (new Char[] { ' ', '"' });
+				if (tmp == "")
+					tmp = "Unknow";
+				author [0] = tmp;
+				tmp = card_json ["author"][1].ToString ().Trim (new Char[] { ' ', '"' });
+				if (tmp == "")
+					tmp = "Unknow";
+				author [1] = tmp;
+
+			}
+
+
+			//Debug.Log (titleob);
+
+
+
+			Texture[] testTexture = Resources.LoadAll<Texture>("picture/"+path);
+			card.name = "Card_"+card_json ["id"].ToString();
+
+			card.transform.Find ("Title").GetComponent<Text> ().text = title;
+			card.transform.Find ("Description").GetComponent<Text> ().text = description;
+			card.transform.Find ("painting").GetComponent<RawImage> ().texture = testTexture [0];
+			card.GetComponent<ShowInformation> ().informationList = informationList;
+			card.GetComponent<ShowInformation> ().authoPainting = author;
+			card.GetComponent<ShowInformation> ().namePainting = titles;
+			card.GetComponent<ShowInformation> ().name = card.name;
+			card.GetComponent<ShowInformation> ().titleCard = title;
+			card.GetComponent<ShowInformation> ().cardButton = card.GetComponent<Button>();
+			//card.GetComponent<ShowInformation> ().type = type;
+
+
+
+
+			card.transform.SetParent(deckList.GetComponentInChildren<ScrollRect>().content,false);
+
+		}
+			
+	}
+
+
+}
---
+++ Assets/Source/Menu/ShowInformation.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class ShowInformation : MonoBehaviour {
+	public Button cardButton;
+	public string titleCard;
+	public string name;
+	private string type;
+	public string[] namePainting;
+	public string[] authoPainting;
+	public GameObject informationList;
+	public GameObject h1preb;
+	public GameObject h2preb;
+	public GameObject h3preb;
+	public GameObject parpreb;
+	public GameObject content;
+
+	private string url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=";
+	private string infPaint = "";
+	private string infAuth = "";
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		cardButton.onClick.AddListener (testOnClick);
+		LoadInformation ();
+	
+	
+	}
+
+	void LoadInformation(){
+
+
+
+
+
+		GameObject imgHeading =  (GameObject)Instantiate (h2preb,new Vector3(0,0,0), Quaternion.identity);  
+		GameObject element = (GameObject)Instantiate (content,informationList.transform.position, informationList.transform.rotation);
+		GameObject title = (GameObject)Instantiate (h1preb, new Vector3(0,0,0), Quaternion.identity);
+		GameObject  headingPaint =(GameObject)Instantiate (h3preb,new Vector3(0,0,0), Quaternion.identity);
+		GameObject desPainting = (GameObject)Instantiate (parpreb,new Vector3(0,0,0), Quaternion.identity);
+		StartCoroutine (requestInformation (namePainting[0], infPaint));
+
+
+		element.name = name;
+
+		title.GetComponent<Text> ().text = titleCard;
+		imgHeading.GetComponent<Text>().text = "Image information";
+		desPainting.GetComponent<Text> ().text = infPaint;
+		//headingPaint.GetComponent<Text> ().text = namePainting [0];
+
+		title.transform.SetParent (element.transform,false);
+		imgHeading.transform.SetParent (element.transform, false);
+		headingPaint.transform.SetParent (element.transform, false);
+			
+
+
+
+
+
+		
+	
+		element.transform.SetParent (informationList.GetComponentInChildren<ScrollRect> ().content, false);
+
+
+		element.SetActive (false);
+
+	}
+
+	void testOnClick(){
+		Debug.Log ("clicked");
+	}
+
+
+
+	IEnumerator requestInformation(string str, string infPaint) {
+		str = str.Replace (" ","_");
+		url = url + str;
+		Debug.Log (url);
+		WWW www = new WWW(url);
+
+		//Debug.Log (www.text);
+		JSONObject jso = new JSONObject (www.text);
+		//JSONObject id =  jso["query"]["pages"][0];
+		infPaint = jso["query"]["pages"][0]["extract"].ToString();
+		yield return www;
+		//Debug.Log (jso ["query"] ["pages"] [id] ["extract"].ToString());
+
+	}
+	
+
+}
