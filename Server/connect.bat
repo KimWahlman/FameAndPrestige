c872f57ab3ea915f0cc0a733fd62c32f3f7d8287
Abelarm <luigi3000@gmail.com>
2016-12-10 16:20:10 +0100

    Added the key and the scripts for connecting to the server

---
+++ Server/connect.bat
@@ -0,0 +1,1 @@
+putty.exe -ssh ec2-user@ec2-52-48-140-226.eu-west-1.compute.amazonaws.com -i fameprestige.pem
---
+++ Server/connect.sh
@@ -0,0 +1,1 @@
+ssh -i fameprestige.pem ec2-user@ec2-52-48-140-226.eu-west-1.compute.amazonaws.com
